57b1459dbbdb15a6544d97acc2b076faf6dcdbd3
Pius Polocha <117569002+Piuscoding@users.noreply.github.com>
2025-02-16 23:16:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 
-app.get("/admin/adminlogs", async(req, res)=>{
+app.get("/admin/adminlons", async(req, res)=>{
   let perPage = 12;
   let page = req.query.page || 1;
 
@@ -69,7 +69,7 @@ app.post("/Create", async(req, res)=>{
     mstatus: req.body.mstatus,
   });
   await PayTracker.create(newPaytracker);
-  res.redirect("/admin/adminlogs");
+  res.redirect("/admin/adminlons");
 })
 
 
@@ -151,7 +151,7 @@ app.put("/edit/:id", async(req, res)=>{
 app.delete("/delete/:id", async(req, res)=>{
   try {
     await PayTracker.deleteOne({ _id: req.params.id });
-    res.redirect("/admin/index");
+    res.redirect("/admin/adminlons");
   } catch (error) {
     console.log(error);
   }
