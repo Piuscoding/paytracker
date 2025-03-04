9b0cb92206f2db4fbc9ea31ab693a8c1fd0d263b
Pius Polocha <117569002+Piuscoding@users.noreply.github.com>
2025-03-04 01:00:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 
-app.get("/admin/adminlons", async(req, res)=>{
+app.get("/admin/adminlogs", async(req, res)=>{
   let perPage = 12;
   let page = req.query.page || 1;
 
